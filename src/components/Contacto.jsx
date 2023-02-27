00f394ceb079069c376badeb600acb340db1b1cf
Dieguinio08 <dieguinio08@gmail.com>
2023-02-27 10:22:26 -0300

    [edit] main.yml

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '@styles/contacto.css';
-
+import ReactWhatsapp from 'react-whatsapp';
+import whatsapp from '@images/whatsapp.svg';
 const Contacto = () => {
     return (
-        <p className='contacto'>Contacto +54 291 4146390</p>
+        <div className='contacto'>
+        <ReactWhatsapp element="div" number="542914146390" message="Hola, te escribo por el gato!">  
+                <img src={whatsapp} alt="" />
+            </ReactWhatsapp> 
+        <p >Contacto +54 291 4146390</p>
+        </div>
     )
 };
 export default Contacto;
