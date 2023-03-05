24bb55e68dfdde1008bf42e12a95b817daad3be7
Dieguinio08 <dieguinio08@gmail.com>
2023-03-05 20:31:43 -0300

    [add*] dirección

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    
+    return (
+        <div  className="banner" >
+            <p>Mitre 1338</p>
+        </div>
+    )
+};
+export default Banner;
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -5,10 +5,11 @@ import whatsapp from '@images/whatsapp.svg';
 const Contacto = () => {
     return (
         <div className='contacto'>
-        <ReactWhatsapp element="div" number="542914146390" message="Hola, te escribo por el gato!">  
+            <ReactWhatsapp element="div" number="542914146390" message="Hola, te escribo por el gato!">  
                 <img src={whatsapp} alt="" />
             </ReactWhatsapp> 
-        <p >Contacto +54 291 4146390</p>
+            <p >Contacto +54 291 4146390</p>
+            <p>Mitre 1338</p>
         </div>
     )
 };
