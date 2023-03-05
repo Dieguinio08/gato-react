39ff29f4eb07f1975cc92734c1262059ae6dfd29
Dieguinio08 <dieguinio08@gmail.com>
2023-03-05 01:29:19 -0300

    [edit] main container

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -3,16 +3,22 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 import MenuContext from '@context/MenuContext';
 import useOculto from '@hooks/useOculto';
-
+import '@styles/main.css';
 const Layout = ({children}) => {
     const oculto=useOculto();
     return (
-        <div>
-            <MenuContext.Provider value={oculto}>
-                <Header/> 
-            </MenuContext.Provider>
-            {children}
-            <Footer/>
+        <div className='main'>
+            <div className='cabecera'>
+                <MenuContext.Provider value={oculto}>
+                    <Header/> 
+                </MenuContext.Provider>
+            </div>
+            <div className='cuerpo'>
+                {children}
+            </div>
+            <div className='pie'>
+                <Footer/>
+            </div>
         </div>
     )
 };
