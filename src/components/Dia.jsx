27f0b94d242b247d9ad01d33c2dca3aabe94c10e
Dieguinio08 <dieguinio08@gmail.com>
2023-03-21 00:39:21 -0300

    modificado medicamentos

--- src/components/Dia.jsx
+++ src/components/Dia.jsx
@@ -9,9 +9,8 @@ const Dia = ({dia}) => {
         <div  className={d ?"dia hoy":"dia"} >
             <p>{dia.nro}</p>
             <ul>
-                {dia.remedios[0] ? <li>Prednisol 15ml <img src={gota} /><img src={gota} /><img src={gota} /></li>:""}
+                {dia.remedios[0] ? <li>Prednisol 15ml <div><img src={gota} /><img src={gota} /><img src={gota} /></div></li>:""}
                 {dia.remedios[1] ?<li>Lasix 40mg <div className="lasix"><Pastilla/></div></li>:""}
-                <li>.</li>
                 {dia.remedios[2] ?<li>Aspirineta <div className='aspirineta'><Pastilla/></div></li>:""}
                 {dia.remedios[3] ?<li>Cardial B 2,5 mg <div className='cardial'><Pastilla/></div></li>:""}
             </ul> 
